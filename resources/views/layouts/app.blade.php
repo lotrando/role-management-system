278c54082cf4f9348b93e4acf1a750b3f01870b1
Lotrando <miroslav.klika@seznam.cz>
2022-09-21 17:57:10 +0200

    Add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name') }}</title>
+  <link type="image/png" href="{{ asset('img/favicon.png') }}" rel="icon">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
